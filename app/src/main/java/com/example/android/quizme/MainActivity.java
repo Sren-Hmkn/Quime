29b9e06a613393e3a0076c86baef3e6b346a02ac
Sren-Hmkn <elias.soeren@gmail.com>
2017-12-23 21:35:09 +0100

    setup the first strings and a small tryout of changing texts

--- app/src/main/java/com/example/android/quizme/MainActivity.java
+++ app/src/main/java/com/example/android/quizme/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.quizme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initQuestions();
+    }
+
+    String[][] myStringArray;
+
+    private void initQuestions(){
+        myStringArray = new String[][] {
+                {getString(R.string.question_text0),getString(R.string.q0_answerA),getString(R.string.q0_answerB),getString(R.string.q0_answerC),getString(R.string.q0_answerD)},
+                {getString(R.string.question_text1),getString(R.string.q1_answerA),getString(R.string.q1_answerB),getString(R.string.q1_answerC),getString(R.string.q1_answerD)},
+                {getString(R.string.question_text2),getString(R.string.q2_answerA),getString(R.string.q2_answerB),getString(R.string.q2_answerC),getString(R.string.q2_answerD)},
+                {getString(R.string.question_text3),getString(R.string.q3_answerA),getString(R.string.q3_answerB),getString(R.string.q3_answerC),getString(R.string.q3_answerD)}
+        };
+    }
+
+    public void onClick(View view) {
+        questionSetup(0,1);
+    }
+
+    private void questionSetup(int questionNumber, int type){
+
+
+        TextView questionView = (TextView) findViewById(R.id.question_view);
+        TextView questionTextView = (TextView) findViewById(R.id.question_textview);
+        CheckBox answerViewA = (CheckBox) findViewById(R.id.answerA);
+        CheckBox answerViewB = (CheckBox) findViewById(R.id.answerB);
+        CheckBox answerViewC = (CheckBox) findViewById(R.id.answerC);
+        CheckBox answerViewD = (CheckBox) findViewById(R.id.answerD);
+
+        questionView.setText(getString(R.string.question_header) + " " + (questionNumber+1));
+        questionTextView.setText(myStringArray[questionNumber][0]);
+        answerViewA.setText(myStringArray[questionNumber][1]);
+        answerViewB.setText(myStringArray[questionNumber][2]);
+        answerViewC.setText(myStringArray[questionNumber][3]);
+        answerViewD.setText(myStringArray[questionNumber][4]);
+
     }
 
 
